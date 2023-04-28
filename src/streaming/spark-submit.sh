29eb3196e86834398646c500ea28f97cb7d41329
Franklyne-kibet <rootkibet@gmail.com>
2023-04-28 16:18:17 +0000

    streaming files

--- src/streaming/spark-submit.sh
+++ src/streaming/spark-submit.sh
@@ -14,9 +14,10 @@ else
 fi
 
 spark-submit --master spark://localhost:7077 --num-executors 2 \
-	           --executor-memory $EXEC_MEM --executor-cores 1 \
-             --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.3.1 \
-            --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions\
-			--conf spark.driver.bindAddress=0.0.0.0 \
-            --conf spark.executorEnv.SPARK_HOME=/bin/spark-3.3.1-bin-hadoop3 \
-            $PYTHON_JOB
+    --executor-memory $EXEC_MEM --executor-cores 1 \
+    --packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.3.1,com.datastax.spark:spark-cassandra-connector_2.12:3.3.0,org.apache.spark:spark-streaming-kafka-0-10_2.12:3.3.1 \
+    --conf spark.sql.extensions=com.datastax.spark.connector.CassandraSparkExtensions \
+    --conf spark.driver.bindAddress=0.0.0.0 \
+    --conf spark.executorEnv.SPARK_HOME=/bin/spark-3.3.1-bin-hadoop3 \
+    --jars /home/franklyne/spark/jars/postgresql-42.6.0.jar \
+    $PYTHON_JOB
--- src/streaming/streaming.py
+++ src/streaming/streaming.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, from_json
+from pyspark.sql.functions import *
 
 from settings import KAFKA_TOPIC,TRAIN_SCHEMA
 
@@ -25,10 +26,10 @@ def parse_train_from_kafka_message(df_raw, schema):
 
     # Flatten the nested columns and select the required columns
     df_flattened = df_json.selectExpr(
-        "CAST(train.sequence_number AS LONG) AS sequence_number",
-        "CAST(train.schedule_id AS LONG) AS schedule_id",
+        "train.sequence_number",
+        "train.schedule_id",
         "train.unique_id",
-        "TO_DATE(train.service_start_date) AS service_start_date",
+        "train.service_start_date",
         "train.location_code",
         "train.scheduled_arrival",
         "train.scheduled_departure",
@@ -47,12 +48,52 @@ def write_cassandra(df):
             .foreachBatch(lambda batchDF, epochId: batchDF.write \
             .format("org.apache.spark.sql.cassandra") \
             .mode("append") \
-            .options(table="service_performance", keyspace="train_service") \
+            .options(table="train_schedule", keyspace="train_service") \
             .save()) \
             .start()
         return write_query
     else:
         print("Data is not streaming")
+        
+def clean_data(df):
+    # Apply data cleaning transformations
+    cleaned_df = df \
+        .withColumn("sequence_number", col("sequence_number").cast("long")) \
+        .withColumn("schedule_id", col("schedule_id").cast("long")) \
+        .withColumn("unique_id", col("unique_id").cast("string")) \
+        .withColumn("service_start_date", col("service_start_date").cast("date")) \
+        .withColumn("location_code", col("location_code").cast("string")) \
+        .withColumn("scheduled_arrival", col("scheduled_arrival").cast("timestamp")) \
+        .withColumn("scheduled_departure", col("scheduled_departure").cast("timestamp")) \
+        .withColumn("actual_arrival", when(col("actual_arrival") == "null", None).otherwise(col("actual_arrival")).cast("timestamp")) \
+        .withColumn("actual_departure", when(col("actual_departure") == "null", None).otherwise(col("actual_departure")).cast("timestamp")) \
+        .withColumn("platforms", when(col("platforms") == "null", None).otherwise(col("platforms")).cast("string")) \
+        .withColumn("estimated_time", when(col("estimated_time") == "null", None).otherwise(col("estimated_time")).cast("timestamp")) \
+        .withColumn("source", when(col("source") == "null", None).otherwise(col("source")).cast("string"))
+
+    return cleaned_df
+
+def write_postgres(df, epoch_id):
+    # Define PostgreSQL connection properties
+    postgres_properties = {
+        "user": "root",
+        "password": "root",
+        "driver": "org.postgresql.Driver",
+        "url": "jdbc:postgresql://172.19.0.7:5432/train_service",
+        "batchsize": "10000"
+    }
+
+    # Define PostgreSQL table name
+    table_name = "train_schedule"
+
+    # Write the data to PostgreSQL using the batch insert mode
+    df.write.jdbc(
+        url=postgres_properties["url"],
+        table=table_name,
+        mode="append",
+        properties=postgres_properties,
+    )
+
 
 def sink_console(df, output_mode: str = 'complete', processing_time: str = '5 seconds'):
     write_query = df.writeStream \
@@ -67,7 +108,7 @@ if __name__ == "__main__":
     spark = SparkSession \
         .builder \
         .appName("Spark-Trains") \
-        .config("spark.cassandra.connection.host", "172.19.0.2") \
+        .config("spark.cassandra.connection.host", "172.19.0.5") \
         .config("spark.cassandra.connection.port", "9042") \
         .config("spark.cassandra.output.consistency.level", "ONE") \
         .getOrCreate()
@@ -90,6 +131,15 @@ if __name__ == "__main__":
     
     # Write the data to cassandra db
     write_cassandra(df_trains)
+    
+    # Clean Data Frame
+    df_cleaned = clean_data(df_trains)
+    
+    # Write the data to Postgres database
+    streaming_df = df_cleaned \
+        .writeStream \
+        .foreachBatch(write_postgres) \
+        .start()
     
     # Sink the data to console
     sink_console(df_trains, output_mode='append')
